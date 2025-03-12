b6995b2d17689ddf1c70ca569967e2f29f514976
Rodolfo Alejandro Oquendo Rojas <65622792+rodolfoquendo@users.noreply.github.com>
2025-03-12 17:11:38 -0300

    update core requirement and coverage to 100%

--- README.md
+++ README.md
@@ -1,6 +1,44 @@
 # js-notifications
 
-DEPRECATED IN FAVOR OF <https://github.com/rodolfoquendo/js-notifications>
+## Github repo
 
-just moved to my personal account to grow my personal portfolio
-and is the same thing to have it there or here, but i need the visibility
+<https://github.com/rodolfoquendo/js-notifications>
+
+## NPM repo
+
+<https://www.npmjs.com/package/@rodolfoquendo/js-notifications>
+
+## Why?
+
+We have to keep in mind that all the javascript that is rendered in a browser is public
+So i will start doing all my javascript in the form of npm packages
+
+This is for html notifications, this might be hooked up to an endpoint to push the notification event to the server
+
+## Installing
+
+```bash
+npm i @rodolfoquendo/js-notifications
+```
+
+## How?
+
+```html
+<!DOCTYPE html>
+<html>
+<body>
+    <div id="notifications" data-report-url="https://example.com/path/to/listener" data></div>    
+</html>
+```
+
+## Testing
+
+```bash
+npm install && npm test && open coverage/lcov-report/index.html
+```
+
+### coverage html report
+
+```bash
+open coverage/lcov-report/index.html
+```
--- tests/Notification.test.js
+++ tests/Notification.test.js
@@ -1,23 +1,37 @@
-import Notification from '../lib/Notification.js';
-global.fetch = (url, options) => new Promise((resolve, reject) => {
+import
+  {
+    Notification,
+    _notify
+  } from '../lib/Notification.js';
+global.fetch = ( url, options ) => new Promise( ( resolve, reject ) =>
+{
   let xhr = new XMLHttpRequest();
-
-  // 2. Configuración: solicitud GET para la URL /article/.../load
-  xhr.open(options.method, url);
-
-  // 3. Envía la solicitud a la red
-  xhr.send(options.body);
-
-  // 4. Esto se llamará después de que la respuesta se reciba
-  xhr.onload = function() {
-    if (xhr.status != 200) { // analiza el estado HTTP de la respuesta
-      reject(xhr.status);
-    } else { // muestra el resultado
-      resolve(xhr.response);
+  xhr.open( options.method, url );
+  xhr.send( options.body );
+  xhr.onload = function ()
+  {
+    if ( xhr.status !== 200 )
+    {
+      reject( xhr.status );
+    } else
+    {
+      resolve( xhr.response );
     }
   };
+} );
+
+
+Object.defineProperty(window, 'location', {
+  value: {
+    href: null
+  },
+  writable: true // possibility to override
 });
-test('Notification.full', async () => {
+
+
+
+test( 'Notification.full', async () =>
+{
   let type = "success",
     timeout = 1,
     content = "Hola",
@@ -26,96 +40,145 @@ test('Notification.full', async () => {
   const notification = new Notification(),
     id = notification.getElementId();
   //defaults
-  expect(notification.holder() !== null).toBe(true);
-  expect(id).toBe('notifications-0');
-  expect(notification.getContent()).toBe(null);
-  expect(notification.getType()).toBe(null);
-  expect(notification.getUrl()).toBe(null);
-  expect(notification.getMethod()).toBe("POST");
-  expect(notification.getTimeout()).toBe(5);
-  expect(notification.element()).toBe(null);
-  expect(notification.getUrl()).toBe(null);
+  expect( notification.holder() !== null ).toBe( true );
+  expect( id ).toBe( 'notifications-0' );
+  expect( notification.getContent() ).toBe( null );
+  expect( notification.getType() ).toBe( null );
+  expect( notification.getReportUrl() ).toBe( null );
+  expect( notification.getMethod() ).toBe( "POST" );
+  expect( notification.getTimeout() ).toBe( 5 );
+  expect( notification.element() ).toBe( null );
+  expect( notification.getReportUrl() ).toBe( null );
 
 
-  notification.setTimeout(timeout);
-  notification.setContent(content);
-  notification.setType(type);
-  notification.setMethod(method);
+  notification.setTimeout( timeout );
+  notification.setContent( content );
+  notification.setType( type );
+  notification.setMethod( method );
 
-  expect(notification.getTimeout()).toBe(timeout * 1000);
-  expect(notification.getContent()).toBe(content);
-  expect(notification.getType()).toBe(type);
-  expect(notification.getMethod()).toBe(method)
+  expect( notification.getTimeout() ).toBe( timeout * 1000 );
+  expect( notification.getContent() ).toBe( content );
+  expect( notification.getType() ).toBe( type );
+  expect( notification.getMethod() ).toBe( method )
 
 
   const dto = notification.dto();
-  expect(dto.type).toBe(type)
-  expect(dto.content).toBe(content)
+  expect( dto.type ).toBe( type )
+  expect( dto.content ).toBe( content )
 
   const html = notification.html();
-  expect(html).toBe(`<div class="notification" data-type="${type}" id="${id}">
-            ${content}
-        </div>`);
+  expect( html ).toBe( `<a class="notification d-block" data-type="${ type }" id="${ id }">
+            ${ content }
+        </a>`);
   notification.addHtml();
-  expect(notification.element() !== null).toBe(true);
-
-  if(url !== null){
-    document.querySelector('body').setAttribute('data-report-url',url);
-  }
-  expect(notification.getUrl()).toBe(url);
-  let  response = await notification.addServer();
-  expect(response !== false).toBe(true);
+  expect( notification.element() !== null ).toBe( true );
+  // we have not setup the url yet
+  expect( notification.getReportUrl() ).toBe( null );
+  let response = await notification.reportToServer();
+  expect( response ).toBe( false );
 
 
-  url = "https://rodolfoquendo.com";
-  notification.setUrl(url);
-  expect(notification.getUrl()).toBe(url)
+  notification.setReportUrl( url );
+  expect( notification.getReportUrl() ).toBe( url );
+  expect( notification.getMethod() ).toBe( "POST" );
 
 
-  expect(notification.getItems().length == 0 );
+  expect( notification.getItems().length == 0 );
 
-  expect(notification.addEventListener()).toBe(true);
-  expect(notification.addTimeout()).toBe(true);
+  expect( notification.addEventListener() ).toBe( true );
+  expect( notification.addTimeout() ).toBe( true );
   const add = await notification.add();
-  expect(add.id).toBe(id);
-  expect(add.sent).toBe(true);
-  expect(add.timeout).toBe(true);
-  expect(add.data.type).toBe(type);
-  expect(add.data.content).toBe(content);
-});
-test('Notification.notify', async () => {
+  expect( add.id ).toBe( id );
+  expect( add.sent ).toBe( true );
+  expect( add.timeout ).toBe( true );
+  expect( add.data.type ).toBe( type );
+  expect( add.data.content ).toBe( content );
+}, 30000 );
+test( 'Notification.defaults_from_html_body', async () =>
+{
   let type = "success",
     timeout = 5,
     content = "'Notification.notify",
     url = "https://insigniaeducation.com",
+    method = "GET";
+  document.querySelector( 'body' ).setAttribute( 'data-report-url', url );
+  document.querySelector( 'body' ).setAttribute( 'data-report-method', method );
+  let notification = new Notification( content );
+  notification.holder().removeAttribute( 'data-report-method' );
+  notification.holder().removeAttribute( 'data-report-url' );
+  notification.setTimeout( timeout );
+  notification.setContent( content );
+  notification.setType( type );
+
+  expect( notification.getTimeout() ).toBe( timeout * 1000 );
+  expect( notification.getContent() ).toBe( content );
+  expect( notification.getType() ).toBe( type );
+  expect( notification.getMethod() ).toBe( method );
+  expect( notification.getReportUrl() ).toBe( url );
+  document.querySelector( 'body' ).removeAttribute( 'data-report-method' );
+  document.querySelector( 'body' ).removeAttribute( 'data-report-url' );
+
+}, 30000 );
+test( 'Notification.defaults_from_html_holder', async () =>
+{
+  let type = "success",
+    timeout = 5,
+    content = "'Notification.notify",
+    url = "https://insigniaeducation.com",
+    method = "GET";
+  let notification = new Notification( content );
+  notification.setTimeout( timeout );
+  notification.setContent( content );
+  notification.setType( type );
+
+  notification.holder().setAttribute( 'data-report-url', url );
+  notification.holder().setAttribute( 'data-report-method', method );
+
+  expect( notification.getReportUrl() ).toBe( url );
+  expect( notification.getMethod() ).toBe( method );
+  notification.holder().removeAttribute( 'data-report-method' );
+  notification.holder().removeAttribute( 'data-report-url' );
+
+}, 30000 );
+test( '_notify', async () =>
+{
+  let type = "success",
+    timeout = 5,
+    content = "'Notification._notify",
+    url = "https://insigniaeducation.com",
     method = "POST";
-  let notification = await Notification.notify(content);
-  expect(notification.id).toBe('notifications-1');
-  expect(notification.sent).toBe(true);
-  expect(notification.timeout).toBe(true);
-  expect(notification.data.type).toBe(type);
-  expect(notification.data.content).toBe(content);
-  
-});
-test('Notification.timeout', async () => {
+  let notification = await _notify( content );
+  expect( notification.id ).toBe( 'notifications-1' );
+  expect( notification.sent ).toBe( true );
+  expect( notification.timeout ).toBe( true );
+  expect( notification.data.type ).toBe( type );
+  expect( notification.data.content ).toBe( content );
+
+}, 30000 );
+test( 'Notification.timeout', async () =>
+{
   let type = "Notification.timeout",
     timeout = 0.5,
     content = "Hola",
     url = null,
     method = "POST";
-  const notification = await Notification.notify(content, type, timeout, url, method);
-  await new Promise(resolve => setTimeout(resolve, timeout * 2 * 1000));
-  expect(window.timeouts.notifications['notifications-2']).toBe(false);
-  
-});
-test('Notification.clickClose', async () => {
+  const notification = await _notify( content, type, timeout, url, method );
+  await new Promise( resolve => setTimeout( resolve, timeout * 3 * 1000 ) );
+  expect( window.timeouts.notifications[ 'notifications-2' ] ).toBe( false );
+
+}, 30000 );
+test( 'Notification.clickClose', async () =>
+{
   let type = "success",
     timeout = 30,
     content = "Notification.clickClose",
-    url = null,
+    report_url = 'https://insigniaeducation.com',
+    click_url = report_url,
     method = "POST";
-  const notification = await Notification.notify(content, type, timeout, url, method);
-  document.getElementById(notification.id).click();
-  expect(document.getElementById(notification.id)).toBe(null);
-  
-});
+  const notification = await _notify( content, type, timeout, report_url, method, click_url ),
+    notification_id = notification.id,
+    element = document.getElementById( notification_id );
+  element.click();
+  expect( document.getElementById( notification.id ) ).toBe( null );
+
+}, 30000 );
