1fb88b97f8e857748b2baeb8812cb4253564ee7a
Rodolfo Alejandro Oquendo Rojas <65622792+rodolfoquendo@users.noreply.github.com>
2025-03-13 16:31:53 -0300

    fix build

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -2,5 +2,5 @@
 
 const mix = require('laravel-mix');
 mix.setPublicPath('dist')
-    .js('lib/Notification.js', 'dist/rodolfoquendo-notifications.js')
+    .js('index.js', 'dist/rodolfoquendo-notifications.js')
     .sass('scss/index.scss', 'dist/rodolfoquendo-notifications.css');
